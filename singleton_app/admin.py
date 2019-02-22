246321cf725bfeed502d500b7986f64131078008
Jonathan Harker <jon@jon.geek.nz>
2019-02-22 16:46:11 +1300

    Use a singleton ModelAdmin to prevent delete/add actions.

--- singleton_app/admin.py
+++ singleton_app/admin.py
@@ -5,4 +5,20 @@ from django.contrib import admin
 
 from .models import SiteSettings
 
-admin.site.register(SiteSettings)
+
+class SingletonModelAdmin(admin.ModelAdmin):
+    """
+    Prevents Django admin users deleting the singleton or adding extra rows.
+    """
+    actions = None  # Removes the default delete action.
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(SingletonModelAdmin):
+    pass
